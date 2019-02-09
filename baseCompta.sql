d34d5460bcf7b67d36dd59acc6b299729d7b342e
Mathias Ravelojaona <mathiaskun40@gmail.com>
2019-02-09 17:46:38 +0300

    go

--- baseCompta.sql
+++ baseCompta.sql
@@ -1,86 +1,74 @@
-drop database if exists gestion;
-create database gestion;
-  use gestion;
-
-  drop table if exists entreprise;
+create table devise(
+id integer not null auto_increment,
+intitule varchar(10),
+valeur double,
+PRIMARY KEY (id)
+);
 create table entreprise(
-id_entreprise integer not null auto_increment,
-nom_entreprise varchar(50),
+id integer not null auto_increment,
+nom varchar(50),
 adresse varchar(10),
-code_postal integer(3),
-id_devise varchar(10),
+code integer(3),
+devise varchar(10),
 allocCodes integer,
 PRIMARY KEY(id)
 );
-alter table devise add FOREIGN KEY (devise) REFERENCES devise(intitule);
 
-drop table if exists journal;
 create table journal(
+id integer not null auto_increment,
 idEntreprise integer not null,
 code varchar(10),
 intitule varchar(50),
-PRIMARY KEY (idEntreprise,code),
-FOREIGN KEY (idEntreprise) REFERENCES entreprise(id_entreprise)
+PRIMARY KEY (id),
+FOREIGN KEY (idEntreprise) REFERENCES entreprise(id)
 );
 
-drop table if exists plan_comptable;
-create table plan_comptable(
+create table plancomptable(
+id integer not null auto_increment,
 idEntreprise integer not null,
 code varchar(10),
 intitule varchar(50),
-PRIMARY KEY (idEntreprise,code),
-FOREIGN KEY (idEntreprise) REFERENCES entreprise(id_entreprise)
+PRIMARY KEY (id),
+FOREIGN KEY (idEntreprise) REFERENCES entreprise(id)
 );
 
-drop table if exists plan_tiers;
-create table plan_tiers(
+create table plantiers(
+id integer not null auto_increment,
 idEntreprise integer not null,
 numero varchar(50),
 intitule varchar(50),
-type_plan varchar(50),
-compte_collectif varchar(10),
-PRIMARY KEY (numero,idEntreprise),
-FOREIGN KEY (idEntreprise) REFERENCES entreprise(id_entreprise),
-FOREIGN KEY (compte_collectif) REFERENCES plan_comptable(code)
+type varchar(50),
+comptecollectif varchar(10),
+PRIMARY KEY (id),
+FOREIGN KEY (idEntreprise) REFERENCES entreprise(id)
 );
 
-drop table if exists types;
 create table types(
 id integer not null auto_increment,
 nom varchar(50),
 PRIMARY KEY(id)
 );
 
-drop table if exists mvt;
 create table mvt(
-idMvt integer not null auto_increment,
+id integer not null auto_increment,
 codeJournal integer,
 date_Mvt date,
 reference varchar(10),
-numero_compte varchar(10),
-numero_tiers varchar(10),
+numerocompte varchar(10),
+numerotiers varchar(10),
 libelle varchar(50),
 echeance date,
 debit double,
 credit double,
-PRIMARY KEY(idMvt),
-FOREIGN KEY (numero_compte) REFERENCES plan_comptable(code),
-FOREIGN KEY (numero_tiers) REFERENCES plan_tiers(numero)
+PRIMARY KEY(id)
 );
 
-drop table if exists devise;
-create table devise(
-id_devise integer not null auto_increment,
-intitule_devise varchar(10),
-valeur double,
-PRIMARY KEY ()
-);
 
-drop table if exists exercice;
+
 create table exercice(
-id_entreprise integer not null,
+id integer not null auto_increment,
+identreprise integer not null,
 annee integer not null,
-isClotured boolean,
-PRIMARY KEY (id_entreprise,annee),
-FOREIGN KEY (id_entreprise) REFERENCES entreprise(id_entreprise)
+isClotured boolean,PRIMARY KEY(id),
+FOREIGN KEY (identreprise) REFERENCES entreprise(id)
 );
