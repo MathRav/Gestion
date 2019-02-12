c9249f9ab19c4fa8ff184633cf485c79d9882eb4
Andy Mihaja <tyranosorus4204bfmv@gmail.com>
2019-02-12 20:07:05 +0300

    vilany ravitoto

--- baseCompta.sql
+++ baseCompta.sql
@@ -1,3 +1,8 @@
+drop database if exists gestion;
+create database gestion;
+  use gestion;
+
+  drop table if exists entreprise;
 create table entreprise(
 id_entreprise integer not null auto_increment,
 nom_entreprise varchar(50),
@@ -9,6 +14,7 @@ PRIMARY KEY(id)
 );
 alter table devise add FOREIGN KEY (devise) REFERENCES devise(intitule);
 
+drop table if exists journal;
 create table journal(
 idEntreprise integer not null,
 code varchar(10),
@@ -17,6 +23,7 @@ PRIMARY KEY (idEntreprise,code),
 FOREIGN KEY (idEntreprise) REFERENCES entreprise(id_entreprise)
 );
 
+drop table if exists plan_comptable;
 create table plan_comptable(
 idEntreprise integer not null,
 code varchar(10),
@@ -25,6 +32,7 @@ PRIMARY KEY (idEntreprise,code),
 FOREIGN KEY (idEntreprise) REFERENCES entreprise(id_entreprise)
 );
 
+drop table if exists plan_tiers;
 create table plan_tiers(
 idEntreprise integer not null,
 numero varchar(50),
@@ -36,12 +44,14 @@ FOREIGN KEY (idEntreprise) REFERENCES entreprise(id_entreprise),
 FOREIGN KEY (compte_collectif) REFERENCES plan_comptable(code)
 );
 
+drop table if exists types;
 create table types(
 id integer not null auto_increment,
 nom varchar(50),
 PRIMARY KEY(id)
 );
 
+drop table if exists mvt;
 create table mvt(
 idMvt integer not null auto_increment,
 codeJournal integer,
@@ -58,6 +68,7 @@ FOREIGN KEY (numero_compte) REFERENCES plan_comptable(code),
 FOREIGN KEY (numero_tiers) REFERENCES plan_tiers(numero)
 );
 
+drop table if exists devise;
 create table devise(
 id_devise integer not null auto_increment,
 intitule_devise varchar(10),
@@ -65,6 +76,7 @@ valeur double,
 PRIMARY KEY ()
 );
 
+drop table if exists exercice;
 create table exercice(
 id_entreprise integer not null,
 annee integer not null,
