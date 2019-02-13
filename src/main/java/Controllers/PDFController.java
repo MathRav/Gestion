1fb43e3f60b3c515b67ee992d34968e51c9393e7
Andy Mihaja <tyranosorus4204bfmv@gmail.com>
2019-02-13 23:00:44 +0300

    starteer pdf

---
+++ src/main/java/Controllers/PDFController.java
@@ -0,0 +1,34 @@
+package Controllers;
+import DAO.GPDF;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pdf")
+public class PDFController {
+
+    @RequestMapping(value = "/pdfreport", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> testr() throws IOException {
+
+        ByteArrayInputStream bis = GPDF.generer();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bis));
+    }
+}
