5939639530ea91dca6b7c69f4c160ab5c45f0e08
Andi <rasoanaivonytokyandi@gmail.com>
2019-02-13 22:51:36 +0300

    entreprise

---
+++ src/main/java/Model/Exercice.java
@@ -0,0 +1,47 @@
+package Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Exercice {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long identreprise;
+    private int annee;
+    private int isclotured;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdentreprise() {
+        return identreprise;
+    }
+
+    public void setIdentreprise(Long identreprise) {
+        this.identreprise = identreprise;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public int getIsclotured() {
+        return isclotured;
+    }
+
+    public void setIsclotured(int isclotured) {
+        this.isclotured = isclotured;
+    }
+}
