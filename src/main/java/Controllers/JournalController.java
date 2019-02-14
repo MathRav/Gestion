a290263a4fa90e0c3953ea70146fecc2cd83c244
Andi <rasoanaivonytokyandi@gmail.com>
2019-02-14 09:02:44 +0300

    journalController

--- src/main/java/Controllers/JournalController.java
+++ src/main/java/Controllers/JournalController.java
@@ -1,14 +1,16 @@
 package Controllers;
 
-import Model.Journal;
+import DAO.ExerciceDAO;
 import DAO.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
+import Model.*;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/Journal")
@@ -16,12 +18,15 @@ public class JournalController {
     @Autowired
     private JournalRepository journalRepository;
 
+    @Autowired
+    private ExerciceDAO exercice;
+
     @GetMapping("/journal.html")
     public ModelAndView loadView(){
         ModelAndView mv = new ModelAndView("journal");
         mv.addObject("journals",journalRepository.findAll());
         Journal j = new Journal();
-        j.setIdEntreprise(1);
+        j.setIdEntreprise(new Long(1));
         mv.addObject("vao",j);
         return mv;
     }
@@ -53,4 +58,23 @@ public class JournalController {
         return "redirect:/Journal/journal.html";
     }
 
+    @GetMapping("/ExerciceMois")
+    public ModelAndView getExerciceMois(){
+        ArrayList<ExerciceMois> listEM = getExerciceJournalMois();
+        ModelAndView mv = new ModelAndView("exMois");
+        mv.addObject("exMois",listEM);
+        return mv;
+    }
+    public ArrayList<ExerciceMois> getExerciceJournalMois(){
+        long id = 1;
+        Exercice ex = exercice.findById(id).get();
+        List<Journal> listeJournal = journalRepository.findByidEntreprise(ex.getIdentreprise());
+        ArrayList<ExerciceMois> listEM = new ArrayList();
+        for(int i =1;i<=12;i++){
+            for(Journal j : listeJournal){
+                listEM.add(new ExerciceMois(ex,i,j));
+            }
+        }
+        return listEM;
+    }
 }
